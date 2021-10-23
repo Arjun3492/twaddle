d11f4a2885ad6f4a5dad1014e2c80d6daea7f910
Ankit <ankitpaswan192@gmail.com>
2021-10-23 11:04:35 +0530

    dynamic searh bar

--- lib/screens/home/widgets/recent_contacts.dart
+++ lib/screens/home/widgets/recent_contacts.dart
@@ -1,8 +1,24 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:twaddle/models/user.dart';
 
-class RecentContacts extends StatelessWidget {
+class RecentContacts extends StatefulWidget {
+  @override
+  State<RecentContacts> createState() => _RecentContactsState();
+}
+
+class _RecentContactsState extends State<RecentContacts> {
+  TextEditingController _searchController = TextEditingController();
+  final ValueNotifier<bool> _searchable = ValueNotifier<bool>(false);
   final contactList = User.generateUsers();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _searchController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,18 +34,56 @@ class RecentContacts extends StatelessWidget {
               color: Colors.white.withOpacity(0.3),
               shape: BoxShape.circle,
             ),
-            child: Icon(Icons.search, size: 30, color: Colors.white),
+            child: GestureDetector(
+                onTap: () {
+                  if (_searchable.value == false) {
+                    _searchable.value = true;
+                  } else if (_searchable.value == true) {
+                    _searchable.value = false;
+                  }
+                },
+                child: ValueListenableBuilder<bool>(
+                    valueListenable: _searchable,
+                    builder: (context, currentState, child) {
+                      return (currentState == false)
+                          ? Icon(Icons.search, size: 30, color: Colors.white)
+                          : Icon(Icons.cancel, size: 30, color: Colors.white);
+                    })),
           ),
           Expanded(
-            child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => CircleAvatar(
-                      radius: 30,
-                      backgroundImage: AssetImage(contactList[index].iconUrl),
-                    ),
-                separatorBuilder: (_, index) => SizedBox(width: 15),
-                itemCount: contactList.length),
-          ),
+              child: ValueListenableBuilder<bool>(
+                  valueListenable: _searchable,
+                  builder: (context, currentState, child) {
+                    return (currentState == false)
+                        ? ListView.separated(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) => CircleAvatar(
+                                  radius: 30,
+                                  backgroundImage:
+                                      AssetImage(contactList[index].iconUrl),
+                                ),
+                            separatorBuilder: (_, index) => SizedBox(width: 15),
+                            itemCount: contactList.length)
+                        : TextFormField(
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                            controller: _searchController,
+                            decoration: InputDecoration(
+                                suffixIcon: GestureDetector(
+                                    child:
+                                        Icon(Icons.search, color: Colors.white),
+                                    onTap: () {
+                                      // if (_formKey.currentState!.validate()) {
+                                      //   print(_searchController.text);
+                                      //   //remove this
+                                      // }
+                                    }),
+                                border: InputBorder.none,
+                                hintText: "Search...",
+                                hintStyle: TextStyle(color: Colors.white)),
+                          );
+                  })
+            
+              ),
         ],
       ),
     );
